07693a2b2970d133418ba4b03eff31fc6e3e5133
JensManfeldt <je.manfeldt@gmail.com>
2023-09-23 22:55:10 +0000

    Print fns moved to own utils mod

---
+++ src/utils/print_utils.rs
@@ -0,0 +1,21 @@
+use crate::game::game::{Game, NeighbourMatrix, Cell};
+
+pub fn print_neighbour_matrix(matrix: &NeighbourMatrix, size: (usize,usize)){
+    for i in 0..size.0{
+        for j in 0..size.1{
+            print!("{:?}", matrix[(i,j)].value);
+        }
+        print!("\n");
+    } 
+}
+
+pub fn print_game(game: &Game ,size: (usize,usize)) -> String{
+    let mut return_string = "".to_string();
+    for i in 0..size.0{
+        for j in 0..size.1{
+            return_string += &game[(i,j)].value.to_string();
+        }
+        return_string += &'\n'.to_string();
+    } 
+    return_string
+}
